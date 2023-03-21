23b0114504ebe2205b50d41621f50d2393b78741
BuildTools <unconfigured@null.spigotmc.org>
2023-03-21 21:11:38 +0100

    Added a list of all quotes to the index page

--- api/endpoint.js
+++ api/endpoint.js
@@ -1,21 +1,20 @@
 // Define a function "endpoint" that takes a "type" parameter.
 const endpoint = (type) => {
-   // Use a switch statement instead of if-else for better readability and easier future expansion.
-   switch (type) {
-     case 'detail':
-       // Return the Kanye Rest API URL for the "detail" type.
-       return 'https://api.kanye.rest';
- 
-     case 'all':
-       // Return the custom URL for the "all" type.
-       return 'https://github.raw.com/iets';
- 
-     default:
-       // Handle unknown types by returning an empty string or an error message.
-       return '';
-   }
- };
- 
- // Export the "endpoint" function as the module's default export.
- module.exports = endpoint;
- 
+  // Use a switch statement instead of if-else for better readability and easier future expansion.
+  switch (type) {
+    case 'detail':
+      // Return the Kanye Rest API URL for the "detail" type.
+      return 'https://api.kanye.rest';
+
+    case 'all':
+      // Return the custom URL for the "all" type.
+      return 'https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json';
+
+    default:
+      // Handle unknown types by returning an empty string or an error message.
+      return '';
+  }
+};
+
+// Export the "endpoint" function as the module's default export.
+module.exports = endpoint;
--- controller/controller.js
+++ controller/controller.js
@@ -1,24 +1,36 @@
+// Import the API module
 const api = require("../api/api");
 
-const index = (req, res) => {
-  // console.log();
+// Render the index page with all the quotes from the API
+const index = async (req, res) => {
+  try {
+    // Fetch all the quotes from the 'all' endpoint of the API
+    const quotes = await api.get("all");
 
-  // Alles wat in const generator staat \/ Hieronder dus
-  // Ook hierin, alleen dan voor het renderen van alle quotes. 
-  // Vooral naar hieronder kijken.
-  
-   res.render('pages/index', {/* Hier voor quotes / vars */});
- };
+    // Render the 'pages/index' template with the fetched quotes
+    res.render('pages/index', { quotes });
+  } catch (err) {
+    // Handle errors by throwing a new Error with the caught error
+    throw new Error(err);
+  }
+};
 
- const generator = (req, res) => {
-  api.get("detail")
-  .then(quote => {res.render('pages/generator', {quote})})
-  .catch(err => {throw new Error})
-  //  res.render('pages/generator', {/* Hier voor quotes / vars */});
- };
+// Render the generator page with a random quote from the API
+const generator = async (req, res) => {
+  try {
+    // Fetch a quote from the 'detail' endpoint of the API
+    const quote = await api.get("detail");
 
+    // Render the 'pages/generator' template with the fetched quote
+    res.render('pages/generator', { quote });
+  } catch (err) {
+    // Handle errors by throwing a new Error with the caught error
+    throw new Error(err);
+  }
+};
 
- module.exports = {
-   index,
-   generator,
- }
+// Export the controller functions
+module.exports = {
+  index,
+  generator,
+};
