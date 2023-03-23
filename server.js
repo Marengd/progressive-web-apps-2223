9c3c997158eb7bac13e2d68b168c95ea48dc72cf
BuildTools <unconfigured@null.spigotmc.org>
2023-03-24 00:18:16 +0100

    Trying to fix a Webpack issue on Mac

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 // Middleware
 
 // Serve static files from the 'public' folder
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 // Serve the 'public/dist' folder as static content under the '/dist' path
 app.use('/dist', express.static('public/dist'));
