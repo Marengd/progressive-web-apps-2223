839415065fdbd802638bbe40863b6d3d71d01fd1
BuildTools <unconfigured@null.spigotmc.org>
2023-03-23 23:23:58 +0100

    Added custom properties and reset to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,36 @@
-// Import required modules
 const path = require("path");
-
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// Set the mode based on the environment variable or default to 'development'
 const mode = process.env.NODE_ENV || 'development';
 
-// Export the configuration object
 module.exports = {
-  mode, // Set the mode ('development' or 'production')
-  entry: "./public/js/app.js", // Specify the entry point for the application
+  mode,
+  entry: {
+    main: './public/js/app.js',
+    'custom-properties': './public/css/custom-properties.css',
+    reset: './public/css/reset.css' // Add 'reset.css' as an entry point
+  },
   output: {
-    path: path.resolve(__dirname, "./public/dist"), // Set the output directory
-    filename: "bundle.js", // Set the output filename
-    publicPath: "/dist/", // Specify the public URL for the output directory
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/dist/'
   },
   module: {
     rules: [
       {
-        test: /\.css$/, // Match all CSS files
-        use: [MiniCssExtractPlugin.loader, "css-loader"], // Use the MiniCssExtractPlugin and css-loader
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(woff|woff2)$/, // Match all font files with .woff or .woff2 extensions
-        type: "asset/resource", // Load fonts as resources
-      },
-    ],
+        test: /\.(woff|woff2)$/,
+        type: 'asset/resource'
+      }
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "style.css", // Set the output filename for the extracted CSS
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, './public'),
-      watch: true,
-    },
-    host: 'localhost',
-    port: 8080,
-  },
-
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
 };
-
