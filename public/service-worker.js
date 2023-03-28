7da6cb1f9e45645af0f745c0c7700ea2ca69ad84
BuildTools <unconfigured@null.spigotmc.org>
2023-03-28 15:02:30 +0200

    Added Redirection To Offline Page, When There Is No Internet Connection

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,42 @@
+// Define the cache name for the service worker
 const cacheName = 'kanye-west-quote-generator-app-cache';
 
 // List of assets to cache
 const urlsToCache = [
-  '/views/pages/offline.ejs'
+  '/offline',
 ];
 
+// Install event listener for the service worker
 self.addEventListener('install', (event) => {
   console.log('Service Worker: installed');
 
+  // Cache the assets during the installation process
   event.waitUntil((async () => {
     try {
+      // Open the cache with the specified cache name
       const cache = await caches.open(cacheName);
       console.log('Opened cache.');
-      return cache.addAll(urlsToCache);
+      
+      // Add the assets to the cache
+      const cacheResult = await cache.addAll(urlsToCache);
+      console.log('Cached URLs:', urlsToCache);
+      return cacheResult;
     } catch (error) {
       console.error('Cache Add All error:', error);
     }
   })());
 });
 
+// Activate event listener for the service worker
 self.addEventListener('activate', (event) => {
   console.log('Service Worker: Activated');
 
+  // Clean up old caches during the activation process
   event.waitUntil((async () => {
+    // Get the list of cache names
     const cacheNames = await caches.keys();
     return Promise.all(
+      // Delete caches that are not the current cache
       cacheNames.filter((thisCacheName) => thisCacheName !== cacheName)
         .map((cacheName) => {
           console.log('Service Worker: Deleting Cache ', cacheName);
@@ -34,10 +46,12 @@ self.addEventListener('activate', (event) => {
   })());
 });
 
+// Fetch event listener for the service worker
 self.addEventListener('fetch', (event) => {
+  // Respond to fetch events with a cached response or a network request
   event.respondWith((async () => {
     try {
-      // Try to match the request in the cache
+      // Try to find a cached response for the request
       const cachedResponse = await caches.match(event.request);
       if (cachedResponse) {
         return cachedResponse;
@@ -57,8 +71,8 @@ self.addEventListener('fetch', (event) => {
       // If response is not valid, return it without caching
       return networkResponse;
     } catch (error) {
-      // If a network error occurs, serve the offline page
-      return caches.match('/offline.ejs');
+      // If a network error occurs, serve the offline page for all requests
+      return caches.match('/offline');
     }
   })());
 });
